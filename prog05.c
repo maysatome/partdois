5123ff92bd7af567916467427979d0a5467dd987
maysatome <47578639+maysatome@users.noreply.github.com>
2019-02-20 19:11:39 +0100

    Create prog05.c

---
+++ prog05.c
@@ -0,0 +1,14 @@
+// Fazer um programa em "C" que solicite 2 números e informe:
+// a) A soma dos números;
+// b) O produto do primeiro número pelo quadrado do segundo;
+// c) O quadrado do primeiro número;
+// d) A raiz quadrada da soma dos quadrados;
+// e) O seno da diferença do primeiro número pelo segundo;
+// f) O módulo do primeiro número.
+
+#include <stdio.h>
+
+int main ()
+{
+
+}
