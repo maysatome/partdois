e2ef9e17a3a18c5581bce1a300fec00492c55ba5
maysatome <47578639+maysatome@users.noreply.github.com>
2019-02-20 17:40:12 +0100

    Create prog03.c

---
+++ prog03.c
@@ -0,0 +1,11 @@
+// Fazer um programa que solicita um número decimal e imprime o correspondente em hexa e octal.
+
+#include <stdio.h>
+
+int main ()
+
+{
+int decimal_base_number = 3;
+
+printf();
+}
