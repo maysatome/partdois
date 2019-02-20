f07327b133623b6bcee8d61892f292eda6976c41
maysatome <47578639+maysatome@users.noreply.github.com>
2019-02-20 14:53:36 +0100

    Update prog02.c

--- prog02.c
+++ prog02.c
@@ -1,21 +1,19 @@
-
 #include <stdio.h>
-#include <stdlib.h>
 //2. Make a program in C that prints a table with the table of 1 to 9
 int main ()
 {
 
- printf ("valor sera %d\n", 1);
- printf ("valor sera %d\n", 2);
- printf ("valor sera %d\n", 3);
- printf ("valor sera %d\n", 4);
- printf ("valor sera %d\n", 5);
- printf ("valor sera %d\n", 6);
- printf ("valor sera %d\n", 7);
- printf ("valor sera %d\n", 8);
- printf ("valor sera %d\n", 9);
- printf ("valor sera %d\n", 10);
- 
- 
+int i,j;
+
+for(i = 1; i<10 ; i++ )
+{
+	
+	for(j =1; j<10 ; j++ )	
+	{
+		printf ("%d x %d = %d\n", i,j, i*j);
+	}
+	printf("\n");
+}
  return 0; 
 }
+
