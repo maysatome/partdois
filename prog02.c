a4d64e1f0894723eadb8e9336ca0dd5adb47144c
maysatome <47578639+maysatome@users.noreply.github.com>
2019-02-20 14:28:46 +0100

    Create prog02.c

---
+++ prog02.c
@@ -0,0 +1,10 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+//2. Make a program in C that prints a table with the table of 1 to 9
+int main ()
+{
+
+ printf ("1 valor sera %d\n", 1);
+ return 0; 
+}
