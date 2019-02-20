505b061a21e9c11a8534058ba7a8d230a8d6daad
maysatome <artemmelnykphd@gmail.com>
2019-02-20 18:24:31 +0000

    doc PEMDAS

--- prog05.c
+++ prog05.c
@@ -6,7 +6,16 @@
 // e) O seno da diferença do primeiro número pelo segundo;
 // f) O módulo do primeiro número.
 
+// Создаем программу на «С», которая запрашивает 2 числа и сообщает:
+// а) сумма чисел;
+// б) Произведение первого числа на квадрат второго;
+// в) квадрат первого числа;
+// г) квадратный корень из суммы квадратов;
+// e) синус разности первого числа вторым;
+// g) Модуль первого числа.
+
 // http://www.cplusplus.com/reference/cmath/
+// https://en.wikipedia.org/wiki/Order_of_operations
 // https://www.geeksforgeeks.org/c-library-math-h-functions/
 
 #include <stdio.h>
