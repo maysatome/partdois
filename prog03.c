0af56722be53e657c5a000722c6f649dd4f921eb
maysatome <47578639+maysatome@users.noreply.github.com>
2019-02-20 18:32:08 +0100

    Update prog03.c

--- prog03.c
+++ prog03.c
@@ -1,5 +1,21 @@
 // Fazer um programa que solicita um número decimal e imprime o correspondente em hexa e octal.
 
+/*
+%с	one symbol _ one character
+%d	Tipo inteiro decimal com um sinal.
+%i	Tipo inteiro decimal com um sinal.
+%е	scientific notation
+%Е	scientific notation
+%f	Número decimal de ponto flutuante.
+%о	Um inteiro não assinado octal.
+%s	Uma seqüência de caracteres.
+%u	Número decimal do tipo inteiro sem sinal.
+%х	Um inteiro hexadecimal sem sinal (letra minúscula).
+%Х	Um inteiro hexadecimal sem sinal (letra maiúscula)
+%р	Exibe o valor do ponteiro.
+%%	Выводит символ %
+*/
+
 #include <stdio.h>
 
 int main ()
