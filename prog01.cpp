9af65d4bc4ee90e3c36140a0569af9210df26efb
maysatome <47578639+maysatome@users.noreply.github.com>
2019-02-20 14:01:31 +0100

    Update prog01.cpp

--- prog01.cpp
+++ prog01.cpp
@@ -1,7 +1,11 @@
 //1. Make a program in C that asks for a value in meters and prints the corresponding in decimeters, centimeters and millimeters.
 
 #include<iostream>
+using namespace std;
 
 int main()
 {
+  double valueInMeters;
+  cout<<"Please enter distance it meters ";
+  cin>> valueInMeters;
 }
