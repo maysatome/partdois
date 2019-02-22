04d9410a55a4ce4f4e98e272140d832ec539f883
maysatome <artemmelnykphd@gmail.com>
2019-02-22 08:58:58 +0000

    toto

--- prog05.c
+++ prog05.c
@@ -6,22 +6,55 @@
 // e) O seno da diferença do primeiro número pelo segundo;
 // f) O módulo do primeiro número.
 
-// Создаем программу на «С», которая запрашивает 2 числа и сообщает:
-// а) сумма чисел;
-// б) Произведение первого числа на квадрат второго;
-// в) квадрат первого числа;
-// г) квадратный корень из суммы квадратов;
-// e) синус разности первого числа вторым;
-// g) Модуль первого числа.
-
 // http://www.cplusplus.com/reference/cmath/
 // https://en.wikipedia.org/wiki/Order_of_operations
 // https://www.geeksforgeeks.org/c-library-math-h-functions/
 
 #include <stdio.h>
+#include<math.h>
 
 int main ()
 {
+double a,b;
 
+printf("first number: \n");
+scanf("%lf",&a);
+printf("second number: \n");
+scanf("%lf",&b);
 
+double tmp_res = a+b;
+printf("(a) The sum is %lf\n", tmp_res);
+
+tmp_res = a*pow(b,2);
+printf("(b) The result is %lf\n", tmp_res);
+
+tmp_res = pow(a,2);
+printf("(c) The result is %lf\n", tmp_res);
+
+tmp_res = sqrt(pow(a,2)+pow(b,2));
+printf("(d) The result is %lf\n", tmp_res);
+
+tmp_res = sin(a-b);
+printf("(e) The result is %lf\n", tmp_res);
+
+tmp_res = fabs(a);
+printf("(f) The result is %lf\n", tmp_res);
+
+return 0;
 }
+
+/*
+gitpod /workspace/partdois $ gcc prog05.c -o prog05 -lm
+gitpod /workspace/partdois $ ./prog05
+first number:
+1.2
+second number:
+2.3
+(a) The sum is 3.500000
+(b) The result is 6.348000
+(c) The result is 1.440000
+(d) The result is 2.594224
+(e) The result is -0.891207
+(f) The result is 1.200000
+gitpod /workspace/partdois $
+*/
