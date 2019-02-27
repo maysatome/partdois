c142dd19209370872ab92b1740a370c760c12ddf
maysatome <artemmelnykphd@gmail.com>
2019-02-27 23:13:07 +0000

    ascii

--- group_ii_02.c
+++ group_ii_02.c
@@ -1,1 +1,17 @@
 
+#include <stdio.h>
+#include <stdlib.h>
+
+int main ()
+{
+    // https://pt.wikipedia.org/wiki/ASCII
+    
+    int i;
+
+    for(i = 0; i<=127; i++)
+    {
+        printf ("%d %X %c \n",i, i, i);
+    }
+
+    return 0;
+}
