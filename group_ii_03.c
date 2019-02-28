888658946a09878f1e1a83b0bd3a3de8029f790a
maysatome <47578639+maysatome@users.noreply.github.com>
2019-02-28 15:08:20 +0100

    Create group_ii_03.c

---
+++ group_ii_03.c
@@ -0,0 +1,1 @@
+//Make a "C" program that reads the price of a product and inflates that price by 10% if it is less than 100 and by 20% if it is greater than or equal to 100. NOTE: Do not use the "if" command or the condition operator "?".
