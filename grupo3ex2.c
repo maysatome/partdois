6fd64a92e32bfcf3b0333b6c202a5b25ff7861d2
maysatome <artemmelnykphd@gmail.com>
2019-03-05 21:55:47 +0000

    debugging ex2

--- grupo3ex2.c
+++ grupo3ex2.c
@@ -7,7 +7,7 @@ int main(){
 
   double n1 = 10.7, n2 = 5.3;
 
-  printf("first %lf second %lf soma is %lf",n1,n2,soma(n1,n2));
-  printf("first %lf second %lf soma is %lf",n1,n2,subtracao(n1,n2));
+  printf("first %.4lf second %.4lf soma is %.4lf\n",n1,n2,soma(n1,n2));
+  printf("first %.4lf second %.4lf subtracao is %.4lf\n",n1,n2,subtracao(n1,n2));
   return 0;
 }
