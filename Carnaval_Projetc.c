14a9ef06e4901e13d95475491c7424219e429c1d
maysatome <47578639+maysatome@users.noreply.github.com>
2019-03-05 10:43:12 -0300

    Create Carnaval_Projetc.c

---
+++ Carnaval_Projetc.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main()
+
+{
+	
+int i, j;
+int m;
+     //m = (i+j)/2;
+ 
+//printf ("Digite a primeira nota e clique em Enter: \n");
+//scanf ("%d", &i);
+//printf ("Digite a segunda nota e clique em Enter: \n"); 
+//scanf ("%d", &j);
+//printf ("Para ver  clique em Enter: \n, (%d + %d)/2 = %d", i,j, (i+j)/2, m);
+//scanf ("%d", &m);
+     m = (i+j)/2;
+
+//if (i>j)
+for (i=0; i<=10; i++)
+{
+	
+ printf ("Valor  medio sera = %d, %d, %d, (%d + %d)/2 = %d \n", i,j, (i+j)/2);	
+}
+
+ 
+ //else
+ //if (i<j)
+ //printf ("Valor da segunda nota= %d \n", j);
+ 
+ //else 
+ if (i==50)
+ printf ("Stop run");
+ 
+  
+
+
+
+	return 0;
+}
