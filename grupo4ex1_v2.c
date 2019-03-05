846f410e9b0a91a9b66034346a416a26fb19ca5f
maysatome <artemmelnykphd@gmail.com>
2019-03-05 22:32:18 +0000

    typo fixed, output ajusted

--- grupo4ex1_v2.c
+++ grupo4ex1_v2.c
@@ -1,14 +1,14 @@
 #include<stdio.h>
 
 int mypowrec (int base, int pow){
-  printf("(%d, %d)\n",base,pow);
+  printf("(%d, %d)\n",base,pow);// this line is to illustrate recursive call of the function
   if (pow==0) return 1;
   return base*mypowrec(base,pow-1);
 }
 
 int main(){
 
-    int n = 10, x = 2;
+    int n = 9, x = 2;
 
     printf("%d\n",mypowrec(x,n));
 
