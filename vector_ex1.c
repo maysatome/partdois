42c3a97566cccf7c5a96aad8908dca51ef5570fc
maysatome <artemmelnykphd@gmail.com>
2019-03-06 08:04:07 +0000

    commit

--- vector_ex1.c
+++ vector_ex1.c
@@ -3,28 +3,47 @@
 
 void main(){
   double container[10] = {0.0};
-  in iterator;
-  
-  for (iterator = 0; iterator <10; iterator ++)
+  int i;
+
+  for (i = 0; i <10; i ++)
   {
-    printf("input %d-th value : ", iterator);
+    printf("input %d-th value : ", i+1);
     scanf("%lf", &container[i]);
-  
+
   }
-  
+
   double min = container[0];
-  
-  for (iterator = 1; iterator <10; iterator ++) container[i]<min?min=container[i];
-  
+
+  for (i = 1; i <10; i ++) if(container[i]<min) min=container[i];
+
   printf("The min value is %lf\n", min);
-    
- 
+
+
   double max = container[0];
-  
-  for (iterator = 1; iterator <10; iterator ++) container[i]>max?max=container[i];
- 
+
+  for (i = 1; i <10; i ++) if(container[i]>max) max=container[i];
+
 
   printf("The max value is %lf\n", max);
- 
+
  return;
 }
+
+/* THE OUTPUT
+
+gitpod /workspace/partdois $ ./vector_ex1
+input 1-th value : 0
+input 2-th value : .2
+input 3-th value : 3.4
+input 4-th value : 5
+input 5-th value : 72.4
+input 6-th value : 11
+input 7-th value : 32
+input 8-th value : 4
+input 9-th value : 55.732
+input 10-th value : 3.14
+The min value is 0.000000
+The max value is 72.400000
+gitpod /workspace/partdois $
+
+*/
