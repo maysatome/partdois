e02adee1c97471d378951e2993d432994fa18bc4
maysatome <artemmelnykphd@gmail.com>
2019-03-06 09:22:31 +0000

    totot

---
+++ vector_ex4.c
@@ -0,0 +1,34 @@
+//Do a routine that receives an array of type double
+// and the number of values that must be requested to the user and returns the filled array with the values entered.
+
+// Please use https://en.wikipedia.org/wiki/C_dynamic_memory_allocation
+
+#include<stdio.h>
+#include<stdlib.h>
+
+void main(){
+
+    int i;
+    int user_length = 0;
+    printf("Please, input the array length (length)");
+    while(user_length >0)
+        scanf("%d",&user_length);
+
+    int * array = malloc(user_length * sizeof(double));
+
+
+  for (i = 0; i <user_length; i ++)
+  {
+    printf("input %d-th value : ", i+1);
+    scanf("%lf", &array[i]);
+
+  }
+
+
+ return;
+}
+
+/* THE OUTPUT
+
+
+*/
